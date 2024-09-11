3797ddea508411ea3dc76df3d2ca1618fad721c1
star1024cd <1star1024cd@gmail.com>
2024-09-11 12:31:31 +0900

    advanced 작업 진행중

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,9 @@ function App() {
   const queryClient = useQueryClient();
   const [title, setTitle] = useState("");
   const [views, setViews] = useState(0);
+  const [comment, setComment] = useState("");
 
+  // 포스트 불러오기
   const getPost = async () => {
     const { data } = await axios.get("http://localhost:4001/posts");
     console.log("데이터", data);
@@ -18,6 +20,7 @@ function App() {
     queryFn: getPost,
   });
 
+  // 포스트 추가
   const addPost = async () => {
     await axios.post("http://localhost:4001/posts", {
       title: title,
@@ -40,12 +43,42 @@ function App() {
     },
   });
 
+  // 프로필 불러오기
+  const getProfile = async () => {
+    const { data: profileData } = await axios.get(
+      "http://localhost:4001/profile"
+    );
+    console.log("ㅍㄿ 데이터", profileData);
+    return profileData;
+  };
+
+  const { profileData, profileLoding, profileError } = useQuery({
+    queryKey: ["profile"],
+    queryFn: getProfile,
+  });
+
+  // 댓글
+  const handleViewComment = () => {
+    alert("댓글표시");
+  };
+
+  const handleAddComment = () => {
+    alert("댓글입력");
+  };
+  //
+
   if (isLoding) {
-    return <div>로딩중입니다</div>;
+    return <div>포스트 로딩중입니다</div>;
   }
 
+  if (profileLoding) {
+    return <div>프로필 로딩중입니다</div>;
+  }
   if (isError) {
-    return <div>오류가 발생했습니다</div>;
+    return <div>포스트 오류가 발생했습니다</div>;
+  }
+  if (profileError) {
+    return <div>프로필 오류가 발생했습니다</div>;
   }
 
   return (
@@ -54,15 +87,37 @@ function App() {
       <input value={title} onChange={(e) => setTitle(e.target.value)} />
       <input value={views} onChange={(e) => setViews(e.target.value)} />
       <button onClick={handlePost}>포스팅</button>
+      <hr />
+      <div>
+        {profileData &&
+          profileData.map((profile) => {
+            return (
+              <div key={profile.name}>
+                <p>
+                  <b>이름 : </b>
+                  {profile.name}
+                </p>
+              </div>
+            );
+          })}
+      </div>
       <div>
         {data &&
           data.map((post) => {
             return (
               <div key={post.id}>
                 <p>
-                  <b>타이틀 : </b>
-                  {post.title} / <b>뷰 :</b> {post.views}
+                  <div>
+                    <b>타이틀 : </b>
+                    {post.title} / <b>뷰 :</b> {post.views}
+                    <button onClick={handleViewComment}>댓글보기</button>
+                  </div>
+                  <div>
+                    <input value={comment} />
+                    <button onClick={handleAddComment}>댓글입력</button>
+                  </div>
                 </p>
+                <hr />
               </div>
             );
           })}
